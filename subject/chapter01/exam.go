23a933c8ea50706dfff8458d1e5911f81a40ba57
sakekoba <114719539+sakekoba@users.noreply.github.com>
2022-10-18 10:59:21 +0000

    provided the answer of chapter01

--- subject/chapter01/exam.go
+++ subject/chapter01/exam.go
@@ -1,5 +1,7 @@
 package chapter01
 
+import "github.com/kurupeku/hello-golang/helper"
+
 // 初乗り料金
 const firstPrice = 500
 
@@ -15,5 +17,8 @@ const perDistance = 250
 // 引数に距離を表す文字列、戻り値が通常料金と深夜料金になるように実装してください
 func Taxi(distance string) (int, int) {
 	// TODO: 実装
-	return 0, 0
+	totalDistMeter := helper.ParseDistance(distance)
+	addPriceTimes := (totalDistMeter - firstRideDistance) / perDistance
+	totalNormalPrice := firstPrice + (perPrice * addPriceTimes)
+	return totalNormalPrice, int(float64(totalNormalPrice) * 1.2)
 }
