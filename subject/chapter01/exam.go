51536c72dfadb7267e6447a7081e703163aeb776
daigo1130 <daigo1130@gmail.com>
2022-10-19 00:25:06 +0000

    chapter01 fixed-2

--- subject/chapter01/exam.go
+++ subject/chapter01/exam.go
@@ -16,8 +16,6 @@ const firstRideDistance = 1500
 // 加算される距離
 const perDistance = 250
 
-var tujo, shinya int
-
 // 引数に距離を表す文字列、戻り値が通常料金と深夜料金になるように実装してください
 func Taxi(distance string) (int, int) {
 	// TODO: 実装
@@ -26,10 +24,10 @@ func Taxi(distance string) (int, int) {
 	dist := helper.ParseDistance(distance)
 
 	//通常料金を計算
-	tujo = firstPrice + ((dist - firstRideDistance) / perDistance * perPrice)
+	tsujo := firstPrice + ((dist - firstRideDistance) / perDistance * perPrice)
 
 	//深夜料金を計算　通常料金*1.2
-	shinya = tujo * 12 / 10
+	shinya := tsujo * 12 / 10
 
-	return tujo, shinya
+	return tsujo, shinya
 }
