afebe3a023097f45f2068ee70bf76963218eb63b
knng1 <mm_fcim_0914@yahoo.co.jp>
2022-10-19 09:59:33 +0000

    chapter01 commit

--- subject/chapter01/exam.go
+++ subject/chapter01/exam.go
@@ -1,5 +1,7 @@
 package chapter01
 
+import "github.com/kurupeku/hello-golang/helper"
+
 // 初乗り料金
 const firstPrice = 500
 
@@ -15,5 +17,18 @@ const perDistance = 250
 // 引数に距離を表す文字列、戻り値が通常料金と深夜料金になるように実装してください
 func Taxi(distance string) (int, int) {
 	// TODO: 実装
-	return 0, 0
+	// 受け取ったstring型の数字をメートルかつint型に直す
+	var hoge int
+	hoge = helper.ParseDistance(distance)
+
+	// 通常料金を計算する
+	var x int
+	x = (((hoge - firstRideDistance) / perDistance) * perPrice) + firstPrice
+
+	// 深夜料金は小数点を含む計算をするので、float32型を宣言する
+	var y float32
+	y = float32(x) * 1.2
+
+	// int型で返さないと怒られるので、yはint型に直してreturnを返す
+	return x, int(y)
 }
