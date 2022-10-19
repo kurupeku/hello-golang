203d757c4d73f610821abfb6a27c6a9bd6b52ff4
ryotaro-rks <84486303+ryotaro-rks@users.noreply.github.com>
2022-10-19 01:26:47 +0000

    chapter01

--- subject/chapter01/exam.go
+++ subject/chapter01/exam.go
@@ -1,5 +1,7 @@
 package chapter01
 
+import "github.com/kurupeku/hello-golang/helper"
+
 // 初乗り料金
 const firstPrice = 500
 
@@ -12,8 +14,27 @@ const firstRideDistance = 1500
 // 加算される距離
 const perDistance = 250
 
+func calculateGeneralAmount(distanceM int) int {
+	amount := firstPrice
+	// 加算回数
+	addCount := (distanceM - firstRideDistance) / perDistance
+	amount += perPrice * addCount
+
+	return amount
+}
+
+func calculateLateAmount(generalAmount int) int {
+	// 1.2倍
+	return generalAmount * 12 / 10
+}
+
 // 引数に距離を表す文字列、戻り値が通常料金と深夜料金になるように実装してください
 func Taxi(distance string) (int, int) {
-	// TODO: 実装
-	return 0, 0
+	// メートルに変換
+	distanceM := helper.ParseDistance(distance)
+
+	generalAmount := calculateGeneralAmount(distanceM)
+	lateAmount := calculateLateAmount(generalAmount)
+	
+	return generalAmount, lateAmount
 }
