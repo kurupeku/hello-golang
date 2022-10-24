2899f568b711d75bc86930ed3e4a1e7eb3edcd4c
chmikata <chmikata@gmail.com>
2022-10-24 05:21:42 +0000

    chapter02できました

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -7,10 +7,21 @@ const (
 	Coin010         = 10
 	Coin005         = 5
 	Coin001         = 1
-	TaxRate      float64 = 0.1
+	TaxRate float64 = 0.1
 )
 
 func MinimumCoins(price uint) (count500, count100, count050, count010, count005, count001 uint) {
-	// TODO: 実装
+	amount := price + uint((float64(price) * TaxRate))
+	count500 = amount / Coin500
+	amount %= Coin500
+	count100 = amount / Coin100
+	amount %= Coin100
+	count050 = amount / Coin050
+	amount %= Coin050
+	count010 = amount / Coin010
+	amount %= Coin010
+	count005 = amount / Coin005
+	amount %= Coin005
+	count001 = amount
 	return
 }
