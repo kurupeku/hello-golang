92e2350c18222042a90c05a250103aa1f31f9126
Yoshinori Ohminato <yoshinori.ominato@rakus.co.jp>
2022-10-25 08:50:35 +0900

    helper 関数を使用するように修正 料金の切り上げ計算を廃止

--- subject/chapter01/exam.go
+++ subject/chapter01/exam.go
@@ -1,10 +1,7 @@
 package chapter01
 
 import (
-	"math"
-	"regexp"
-	"strconv"
-	"strings"
+	"github.com/kurupeku/hello-golang/helper"
 )
 
 // 初乗り料金
@@ -23,41 +20,16 @@ const perDistance = 250
 func Taxi(distance string) (int, int) {
 	// TODO: 実装
 	// 距離をメートルに変換
-	var totalDistance = ConvToMeters(distance)
+	var totalDistance int = helper.ParseDistance(distance)
 
 	// 初乗り
 	var normalPrice int = firstPrice
 
 	// 加算分 ※ 初乗りを超えた途端にメーターは上がるので、距離の端数は切り上げ
 	if totalDistance > firstRideDistance {
-		normalPrice += int(math.Ceil(float64(totalDistance-firstRideDistance)/perDistance)) * perPrice
+		normalPrice += (totalDistance - firstRideDistance) / perDistance * perPrice
 	}
 
 	// 計算結果を返す
 	return normalPrice, int(1.2 * float64(normalPrice))
-}
-
-// 距離をメートルに変換する
-func ConvToMeters(distance string) int {
-
-	// 単位当たりの比率
-	var unitRatio = map[string]float64{
-		"mm": 0.001,
-		"cm": 0.01,
-		"m":  1,
-		"km": 1000}
-
-	// 数値と単位を分割
-	rep := regexp.MustCompile(`([\d\.]+)([A-Za-z]*)`)
-
-	// 単位部分を取得
-	var numVal, _ = strconv.ParseFloat(rep.ReplaceAllString(distance, "$1"), 64)
-	var unitStr string = strings.ToLower(rep.ReplaceAllString(distance, "$2"))
-
-	// メートル変換した整数値を返す
-	if len(unitStr) > 0 {
-		return int(numVal * unitRatio[unitStr])
-	} else {
-		return int(numVal)
-	}
 }
