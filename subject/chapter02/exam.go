cf7b2dc59fd536378e13771b214cf6371d322d72
akioh <a.a.10.0326@gmail.com>
2022-10-25 10:20:08 +0000

    tabun dekita

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -7,10 +7,20 @@ const (
 	Coin010         = 10
 	Coin005         = 5
 	Coin001         = 1
-	TaxRate      float64 = 0.1
+	TaxRate float64 = 0.1
 )
 
 func MinimumCoins(price uint) (count500, count100, count050, count010, count005, count001 uint) {
 	// TODO: 実装
+	tax := float64(price) * TaxRate
+	in_tax_price := price + uint(tax)
+	count500 = in_tax_price / Coin500
+	count100 = (in_tax_price % Coin500) / Coin100
+	count050 = (in_tax_price % Coin100) / Coin050
+	count010 = (in_tax_price % Coin050) / Coin010
+	count005 = (in_tax_price % Coin010) / Coin005
+	count001 = (in_tax_price % Coin005) / Coin001
+
 	return
+	// return count500, count100, count050, count010, count005, count001
 }
