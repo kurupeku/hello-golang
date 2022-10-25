bdf5c7cf8e11e311f2bf002c195aa14018c3a75f
kazukiwatanabe21 <kazuki.watanabe@rakus.co.jp>
2022-10-25 11:10:10 +0000

    Yeah!!

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -7,10 +7,27 @@ const (
 	Coin010         = 10
 	Coin005         = 5
 	Coin001         = 1
-	TaxRate      float64 = 0.1
+	TaxRate float64 = 0.1
 )
 
 func MinimumCoins(price uint) (count500, count100, count050, count010, count005, count001 uint) {
-	// TODO: 実装
-	return
+	tax_included_price := uint(float64(price) * (1 + TaxRate))
+	count500 = uint(tax_included_price / Coin500)
+	amari_of_count500 := uint(tax_included_price % Coin500)
+
+	count100 = uint(amari_of_count500 / Coin100)
+	amari_of_count100 := uint(amari_of_count500 % Coin100)
+
+	count050 = uint(amari_of_count100 / Coin050)
+	amari_of_count050 := uint(amari_of_count100 % Coin050)
+
+	count010 = uint(amari_of_count050 / Coin010)
+	amari_of_count010 := uint(amari_of_count050 % Coin010)
+
+	count005 = uint(amari_of_count010 / Coin005)
+	amari_of_count005 := uint(amari_of_count010 % Coin005)
+
+	count001 = uint(amari_of_count005 / Coin001)
+
+	return count500, count100, count050, count010, count005, count001
 }
