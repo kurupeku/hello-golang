f2d6342302f4afd361c68fdfbec01d7f536212b3
Yoshinori Ohminato <yoshinori.ominato@rakus.co.jp>
2022-10-25 20:20:38 +0900

    チャプター２

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -7,10 +7,22 @@ const (
 	Coin010         = 10
 	Coin005         = 5
 	Coin001         = 1
-	TaxRate      float64 = 0.1
+	TaxRate float64 = 0.1
 )
 
 func MinimumCoins(price uint) (count500, count100, count050, count010, count005, count001 uint) {
 	// TODO: 実装
-	return
+	var remainderPrice uint = uint(float64(price) * (1 + TaxRate))
+	count500 = uint(remainderPrice / Coin500)
+	remainderPrice = uint(remainderPrice % Coin500)
+	count100 = uint(remainderPrice / Coin100)
+	remainderPrice = uint(remainderPrice % Coin100)
+	count050 = uint(remainderPrice / Coin050)
+	remainderPrice = uint(remainderPrice % Coin050)
+	count010 = uint(remainderPrice / Coin010)
+	remainderPrice = uint(remainderPrice % Coin010)
+	count005 = uint(remainderPrice / Coin005)
+	count001 = uint(remainderPrice % Coin005)
+
+	return count500, count100, count050, count010, count005, count001
 }
