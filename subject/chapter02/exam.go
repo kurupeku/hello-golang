1a07f162c9706ef3ca5dffdd6b410f4c94d8ba21
ryotaro-rks <84486303+ryotaro-rks@users.noreply.github.com>
2022-10-26 02:30:53 +0000

    chapter2

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -10,7 +10,33 @@ const (
 	TaxRate      float64 = 0.1
 )
 
+// 税込みに変換
+func changeTaxIncluded(price uint) uint {
+	return uint(float64(price) * (TaxRate + 1))
+}
+
+// コインの使用枚数
+func countCoin(price uint, coin uint) uint {
+	return price / coin
+}
+
 func MinimumCoins(price uint) (count500, count100, count050, count010, count005, count001 uint) {
-	// TODO: 実装
+	currentPrice := changeTaxIncluded(price)
+	count500 = countCoin(currentPrice, Coin500)
+
+	currentPrice -= count500 * Coin500
+	count100 = countCoin(currentPrice, Coin100)
+
+	currentPrice -= count100 * Coin100
+	count050 = countCoin(currentPrice, Coin050)
+
+	currentPrice -= count050 * Coin050
+	count010 = countCoin(currentPrice, Coin010)
+
+	currentPrice -= count010 * Coin010
+	count005 = countCoin(currentPrice, Coin005)
+
+	currentPrice -= count005 * Coin005
+	count001 = countCoin(currentPrice, Coin001)
 	return
 }
