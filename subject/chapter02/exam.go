799498fbfe86c9971309b1cde6e4cb7933b1e772
miyama <36231225+hige-dev@users.noreply.github.com>
2022-10-26 06:32:55 +0000

    chapter02

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -7,10 +7,22 @@ const (
 	Coin010         = 10
 	Coin005         = 5
 	Coin001         = 1
-	TaxRate      float64 = 0.1
+	TaxRate float64 = 0.1
 )
 
 func MinimumCoins(price uint) (count500, count100, count050, count010, count005, count001 uint) {
-	// TODO: 実装
+	p := float64(price) * (1 + TaxRate)
+	count500, remain1 := divmod(uint(p), Coin500)
+	count100, remain2 := divmod(remain1, Coin100)
+	count050, remain3 := divmod(remain2, Coin050)
+	count010, remain4 := divmod(remain3, Coin010)
+	count005, remain5 := divmod(remain4, Coin005)
+	count001, _ = divmod(remain5, Coin001)
+	return
+}
+
+func divmod(numerator, denominator uint) (quotient, remainder uint) {
+	quotient = numerator / denominator // integer division, decimals are truncated
+	remainder = numerator % denominator
 	return
 }
