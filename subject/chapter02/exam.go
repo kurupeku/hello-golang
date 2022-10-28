e6cc9da036df281f1ea029236a4a177986fa993b
sakekoba <114719539+sakekoba@users.noreply.github.com>
2022-10-28 03:23:26 +0000

    Answer for Chapter2

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -7,10 +7,21 @@ const (
 	Coin010         = 10
 	Coin005         = 5
 	Coin001         = 1
-	TaxRate      float64 = 0.1
+	TaxRate float64 = 0.1
 )
 
 func MinimumCoins(price uint) (count500, count100, count050, count010, count005, count001 uint) {
 	// TODO: 実装
+	total_price := price + uint(float64(price)*TaxRate)
+	count500 = total_price / Coin500
+	total_price %= Coin500
+	count100 = total_price / Coin100
+	total_price %= Coin100
+	count050 = total_price / Coin050
+	total_price %= Coin050
+	count010 = total_price / Coin010
+	total_price %= Coin010
+	count005 = total_price / Coin005
+	count001 = total_price % Coin005
 	return
 }
