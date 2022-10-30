81d9b7fa0f20cba59a79e7ea46a472253236d69a
miyama <36231225+hige-dev@users.noreply.github.com>
2022-10-30 23:34:42 +0900

    chapter05

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,28 @@
 package chapter05
 
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	if len(daruma) <= 1 {
+		return daruma
+	}
+	var drop int
+	if len(daruma)%2 == 0 {
+		drop = len(daruma)/2 - 1
+	} else {
+		drop = len(daruma) / 2
+	}
+	daruma = append(daruma[:drop], daruma[drop+1:]...)
+	return daruma
 }
 
 func MatrixMultiple(seed []int) [][]int {
-	// TODO: 実装
-	return [][]int{}
+	var result = make([][]int, len(seed))
+	for i, _ := range result {
+		result[i] = make([]int, len(seed))
+	}
+	for i, v1 := range seed {
+		for j, v2 := range seed {
+			result[i][j] = v1 * v2
+		}
+	}
+	return result
 }
