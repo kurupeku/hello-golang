58151e7c529eb9a002ffcbcebc28b40c45b7b0af
miyama <36231225+hige-dev@users.noreply.github.com>
2022-10-30 19:46:10 +0900

    chapter03

--- subject/chapter03/exam.go
+++ subject/chapter03/exam.go
@@ -1,13 +1,84 @@
 package chapter03
 
+import "github.com/kurupeku/hello-golang/helper"
+
 // If を使用して料金の条件分岐を行ってください
 func InnerChargeFromTokyo(station string) int {
-	// TODO: 実装
+	if station == "東京" {
+		return 0
+	}
+
+	var current = "東京"
+	var next string
+	var distance int
+
+	for {
+		if current == station {
+			break
+		}
+		next = helper.InnerNextStation(current)
+		distance += helper.InnerLoopDistance(next)
+		current = next
+	}
+
+	if distance <= 3999 {
+		return 140
+	} else if distance >= 4000 && distance <= 6999 {
+		return 160
+	} else if distance >= 7000 && distance <= 10999 {
+		return 170
+	} else if distance >= 11000 && distance <= 15999 {
+		return 200
+	} else if distance >= 16000 && distance <= 20999 {
+		return 270
+	} else if distance >= 21000 && distance <= 25999 {
+		return 350
+	} else if distance >= 26000 && distance <= 30999 {
+		return 420
+	} else if distance >= 31000 {
+		return 490
+	}
+
 	return 0
 }
 
 // Switch を使用して料金の条件分岐を行ってください
 func OuterChargeFromTokyo(station string) int {
-	// TODO: 実装
-	return 0
+	if station == "東京" {
+		return 0
+	}
+
+	var current = "東京"
+	var next string
+	var distance int
+
+	for {
+		if current == station {
+			break
+		}
+		next = helper.OuterNextStation(current)
+		distance += helper.OuterLoopDistance(next)
+		current = next
+	}
+
+	switch {
+	case distance <= 3999:
+		return 140
+	case distance >= 4000 && distance <= 6999:
+		return 160
+	case distance >= 7000 && distance <= 10999:
+		return 170
+	case distance >= 11000 && distance <= 15999:
+		return 200
+	case distance >= 16000 && distance <= 20999:
+		return 270
+	case distance >= 21000 && distance <= 25999:
+		return 350
+	case distance >= 26000 && distance <= 30999:
+		return 420
+	case distance >= 31000:
+		return 490
+	default:
+		return 0
+	}
 }
