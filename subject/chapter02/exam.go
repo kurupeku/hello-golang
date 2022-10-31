04ddc62f1288f7c22dc9c117e73f1487b30996a4
rks-kairi <84486312+rks-kairi@users.noreply.github.com>
2022-10-31 23:13:03 +0000

    chapter2

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -7,10 +7,21 @@ const (
 	Coin010         = 10
 	Coin005         = 5
 	Coin001         = 1
-	TaxRate      float64 = 0.1
+	TaxRate float64 = 0.1
 )
 
 func MinimumCoins(price uint) (count500, count100, count050, count010, count005, count001 uint) {
-	// TODO: 実装
+	price_with_tax := uint(float64(price) * (TaxRate + 1))
+	count500 = price_with_tax / Coin500
+	price_with_tax -= Coin500 * count500
+	count100 = price_with_tax / Coin100
+	price_with_tax -= Coin100 * count100
+	count050 = price_with_tax / Coin050
+	price_with_tax -= Coin050 * count050
+	count010 = price_with_tax / Coin010
+	price_with_tax -= Coin010 * count010
+	count005 = price_with_tax / Coin005
+	price_with_tax -= Coin005 * count005
+	count001 = price_with_tax / Coin001
 	return
 }
