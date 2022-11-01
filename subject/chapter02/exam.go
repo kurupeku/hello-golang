c48088928da7924ac5b5c46707fd61fc13d591c3
kojikanemoto <kojikanemoto@rakus.co.jp>
2022-11-01 14:53:21 +0900

    by kanemoto

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -11,6 +11,20 @@ const (
 )
 
 func MinimumCoins(price uint) (count500, count100, count050, count010, count005, count001 uint) {
-	// TODO: 実装
-	return
+	var maney int
+	var IncTax float64
+	IncTax = float64(price) * (1 + TaxRate)
+	maney = int(IncTax)
+	count500 = uint(maney / Coin500)
+	maney = maney % Coin500
+	count100 = uint(maney / Coin100)
+	maney = maney % Coin100
+	count050 = uint(maney / Coin050)
+	maney = maney % Coin050
+	count010 = uint(maney / Coin010)
+	maney = maney % Coin010
+	count005 = uint(maney / Coin005)
+	maney = maney % Coin005
+	count001 = uint(maney / Coin001)
+	return count500, count100, count050, count010, count005, count001
 }
