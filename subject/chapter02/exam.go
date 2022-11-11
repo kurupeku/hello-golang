4f83b6c0bcc18b509907989b4b5f1012e1ea4d51
eyes-919 <eyes.works@11069161+eyes-919@users.noreply.github.com>
2022-11-11 07:42:59 +0000

    2022/11/11 一旦完成

--- subject/chapter02/exam.go
+++ subject/chapter02/exam.go
@@ -1,7 +1,5 @@
 package chapter02
 
-import "fmt"
-
 const (
 	Coin500         = 500
 	Coin100         = 100
@@ -16,13 +14,11 @@ func MinimumCoins(price uint) (count500, count100, count050, count010, count005,
 	// TODO: 実装
 	var tmp1 float64
 	var tmp2 uint
-
 	// 結構手抜き
 	tmp1 = float64(price)
 	tmp1 = tmp1 + (tmp1 * TaxRate)
 	tmp2 = uint(tmp1)
-	fmt.Printf("price: %d\n", tmp2)
-
+	// fmt.Printf("price: %d\n", tmp2)
 	// 順繰り順繰り割っては次に渡していく
 	// 500
 	count500 = tmp2 / Coin500
@@ -42,7 +38,6 @@ func MinimumCoins(price uint) (count500, count100, count050, count010, count005,
 	// 1
 	count001 = tmp2 / Coin001
 	tmp2 = tmp2 % Coin001
-
 	// デバッグ出力させてみる
 	// fmt.Printf("500:%d\n100:%d\n50:%d\n10:%d\n5:%d\n1:%d\n", count500, count100, count050, count010, count005, count001)
 	return
--- subject/chapter03/exam.go
+++ subject/chapter03/exam.go
@@ -1,13 +1,91 @@
 package chapter03
 
+import (
+	// "fmt"
+	"github.com/kurupeku/hello-golang/helper"
+)
+
 // If を使用して料金の条件分岐を行ってください
 func InnerChargeFromTokyo(station string) int {
 	// TODO: 実装
-	return 0
+	var start string = "東京"
+	var end string = station
+	var total_distance int
+
+	if start == end {
+		return 0
+	}
+
+	for start != end {
+		total_distance = helper.InnerLoopDistance(start) + total_distance
+		start = helper.InnerNextStation(start)
+	}
+
+	return GetChargeIf(total_distance)
 }
 
 // Switch を使用して料金の条件分岐を行ってください
 func OuterChargeFromTokyo(station string) int {
 	// TODO: 実装
-	return 0
+	var start string = "東京"
+	var end string = station
+	var total_distance int
+
+	if start == end {
+		return 0
+	}
+
+	for start != end {
+		total_distance = helper.InnerLoopDistance(start) + total_distance
+		start = helper.OuterNextStation(start)
+	}
+
+	return getChargeSwitch(total_distance)
+}
+
+func GetChargeIf(distance int) int {
+	var charge int
+	if distance <= 3999 {
+		charge = 140
+	} else if distance <= 6999 {
+		charge = 160
+	} else if distance <= 10999 {
+		charge = 170
+	} else if distance <= 15999 {
+		charge = 200
+	} else if distance <= 20999 {
+		charge = 270
+	} else if distance <= 25999 {
+		charge = 350
+	} else if distance <= 30999 {
+		charge = 420
+	} else {
+		charge = 490
+	}
+
+	return charge
+}
+
+func getChargeSwitch(distance int) int {
+	var charge int
+	switch {
+	case distance <= 3999:
+		charge = 140
+	case distance <= 6999:
+		charge = 160
+	case distance < 10999:
+		charge = 170
+	case distance < 15999:
+		charge = 200
+	case distance <= 20999:
+		charge = 270
+	case distance <= 25999:
+		charge = 350
+	case distance <= 30999:
+		charge = 420
+	case distance >= 31000:
+		charge = 490
+	}
+
+	return charge
 }
