4a93647b74c30443ce31a53e01bec1874cf86058
rks-kairi <84486312+rks-kairi@users.noreply.github.com>
2022-11-14 23:15:37 +0000

    chapter03

--- subject/chapter03/exam.go
+++ subject/chapter03/exam.go
@@ -1,13 +1,82 @@
 package chapter03
 
+import (
+	"github.com/kurupeku/hello-golang/helper"
+)
+
 // If を使用して料金の条件分岐を行ってください
 func InnerChargeFromTokyo(station string) int {
-	// TODO: 実装
-	return 0
+	nextStation := "東京"
+	totalDistance := 0
+	if station == nextStation {
+		return 0
+	}
+	for {
+		nextStation = helper.InnerNextStation(nextStation)
+		totalDistance += helper.InnerLoopDistance(nextStation)
+		if nextStation == station {
+			break
+		}
+	}
+
+	if totalDistance < 4000 {
+		return 140
+	}
+	if totalDistance < 7000 {
+		return 160
+	}
+	if totalDistance < 11000 {
+		return 170
+	}
+	if totalDistance < 16000 {
+		return 200
+	}
+	if totalDistance < 21000 {
+		return 270
+	}
+	if totalDistance < 26000 {
+		return 350
+	}
+	if totalDistance < 31000 {
+		return 420
+	}
+	return 490
 }
 
 // Switch を使用して料金の条件分岐を行ってください
 func OuterChargeFromTokyo(station string) int {
-	// TODO: 実装
-	return 0
+	nextStation := "東京"
+	totalDistance := 0
+
+	switch station {
+	case nextStation:
+		return 0
+	}
+
+	for {
+		nextStation = helper.OuterNextStation(nextStation)
+		totalDistance += helper.OuterLoopDistance(nextStation)
+		if nextStation == station {
+			break
+		}
+	}
+
+	switch {
+	case totalDistance < 4000:
+		return 140
+	case totalDistance < 7000:
+		return 160
+	case totalDistance < 11000:
+		return 170
+	case totalDistance < 16000:
+		return 200
+	case totalDistance < 21000:
+		return 270
+	case totalDistance < 26000:
+		return 350
+	case totalDistance < 31000:
+		return 420
+	default:
+		return 490
+	}
 }
