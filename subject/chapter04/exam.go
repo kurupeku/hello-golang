1e3faab63c60aafb680a54d8fe00f8d7bfaaf45c
ryotaro-rks <84486303+ryotaro-rks@users.noreply.github.com>
2022-11-15 11:00:03 +0000

    chapter04

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -6,6 +6,22 @@ type Card struct {
 }
 
 func Kaisatsu(charge int, card *Card) bool {
-	// TODO: 実装
-	return false
+	// 足りない場合
+	if charge > card.Point + card.Balance {
+		return false;
+	}
+
+	// ポイントで払える場合
+	if charge <= card.Point {
+		card.Point -= charge
+		return true;
+	}
+
+	// ポイントを使い切る
+	charge -= card.Point
+	card.Point = 0
+
+	// 残りを現金で
+	card.Balance -= charge
+	return true
 }
