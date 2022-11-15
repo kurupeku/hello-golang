5cc8e3a584cd0b143b0bdc761b338a1e36358ddc
gumamon <110001528+ninaguma3@users.noreply.github.com>
2022-11-15 19:59:24 +0900

    fixed chapter04 exam.go

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -1,11 +1,29 @@
 package chapter04
 
+import (
+	"fmt"
+)
+
 type Card struct {
 	Balance int
 	Point   int
 }
 
 func Kaisatsu(charge int, card *Card) bool {
-	// TODO: 実装
+	tmp := 0
+
+	switch {
+	case charge < card.Point:
+		card.Point -= charge
+		fmt.Println(card)
+		return true
+	case charge <= card.Balance+card.Point:
+		tmp = charge - card.Point
+		card.Point = 0
+		card.Balance -= tmp
+		fmt.Println(card)
+		return true
+	}
+
 	return false
 }
