bbbd14ed2f6493dbc79e506957aa4199d2c3eb42
sakekoba <114719539+sakekoba@users.noreply.github.com>
2022-11-15 10:45:26 +0000

    Answer for Chapter04

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -7,5 +7,16 @@ type Card struct {
 
 func Kaisatsu(charge int, card *Card) bool {
 	// TODO: 実装
-	return false
+	if charge > (card.Point + card.Balance) {
+		return false
+	} else {
+		if charge > card.Point {
+			charge -= card.Point
+			card.Balance -= charge
+			card.Point = 0
+		} else {
+			card.Point -= charge
+		}
+		return true
+	}
 }
