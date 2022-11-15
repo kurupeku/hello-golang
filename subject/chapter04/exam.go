36586381c2ff639a91c172fd64da1d0ec79682c2
Yoshinori Ohminato <yoshinori.ominato@rakus.co.jp>
2022-11-15 20:08:40 +0900

    チャプター04

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -6,6 +6,18 @@ type Card struct {
 }
 
 func Kaisatsu(charge int, card *Card) bool {
+
+	if charge > (card.Point + card.Balance) {
+		return false
+	}
+
+	if charge > card.Point {
+		card.Balance = card.Balance - charge + card.Point
+		card.Point = 0
+	} else {
+		card.Point -= charge
+	}
+
 	// TODO: 実装
-	return false
+	return true
 }
