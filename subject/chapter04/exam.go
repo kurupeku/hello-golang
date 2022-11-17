0bd3f56313b5b8640a3eb79264bb1e17bffccb65
eyes-919 <eyes.works@11069161+eyes-919@users.noreply.github.com>
2022-11-17 09:39:26 +0000

    2022/11/17 一旦のやっつけ完成

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -1,5 +1,9 @@
 package chapter04
 
+import (
+// "fmt"
+)
+
 type Card struct {
 	Balance int
 	Point   int
@@ -7,5 +11,45 @@ type Card struct {
 
 func Kaisatsu(charge int, card *Card) bool {
 	// TODO: 実装
-	return false
+
+	/*
+		fmt.Printf("乗車料金:%d\n", charge)
+		fmt.Printf("card.Balance:%d\n", card.Balance)
+		fmt.Printf("card.Point:%d\n", card.Point)
+	*/
+
+	var total int
+	var tmp int
+
+	total = card.Balance + card.Point
+
+	// そもそもICカード残高より乗車料金が高いかどうか
+	// 残高以内
+	if total-charge >= 0 {
+		// カードポイントに余りがある場合
+		if card.Point > 0 {
+			tmp = card.Point - charge
+			// fmt.Printf("tmp: %d\n", tmp)
+			if tmp <= 0 {
+				// print("1\n")
+				card.Point = card.Point - charge
+				card.Point = card.Point - tmp
+				card.Balance = card.Balance + tmp
+				// fmt.Printf("end card.Point: %d\n", card.Point)
+				// fmt.Printf("end card.Balance: %d\n", card.Balance)
+				return true
+			} else {
+				// print("2\n")
+				card.Point = card.Point - charge
+				return true
+			}
+			// カードポイント残高0の場合
+		} else {
+			card.Balance = card.Balance - charge
+			return true
+		}
+	} else {
+		// そもそも初めから残高不足だからfalse
+		return false
+	}
 }
