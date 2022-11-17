b57f2ead1d404b401a58e34606330b2ef279601a
knng1 <mm_fcim_0914@yahoo.co.jp>
2022-11-17 10:39:24 +0000

    chap3commit

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -7,5 +7,25 @@ type Card struct {
 
 func Kaisatsu(charge int, card *Card) bool {
 	// TODO: 実装
-	return false
+	var c = card
+	// まずは通れるか通れないかの2パターン
+	// 通れないパターン
+	// falseで抜けるだけ
+	if charge > (c.Point + c.Balance) {
+		return false
+	}
+	// 通れるパターンは2通り
+	if (charge - c.Point) > 0 {
+		/// ポイントだけで払えないパターン処理
+		//// chargeからpoint分を引いた値をBalanceから減算
+		c.Balance = c.Balance + c.Point - charge
+		//// Pointだけで払えないからPointは全部使う
+		c.Point = 0
+		return true
+	} else {
+		/// ポイントだけで払えるパターン処理
+		//// Pointだけで払えるからPointからchargeを引く
+		c.Point = c.Point - charge
+		return true
+	}
 }
