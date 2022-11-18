769affed7ccbcd5adf96a6cd01d541e2e7d9e75a
eyes-919 <eyes.works@11069161+eyes-919@users.noreply.github.com>
2022-11-18 01:29:41 +0000

    2022/11/18 code clean up

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -23,33 +23,42 @@ func Kaisatsu(charge int, card *Card) bool {
 
 	total = card.Balance + card.Point
 
-	// そもそもICカード残高より乗車料金が高いかどうか
-	// 残高以内
+	// 1 そもそもICカード残高より乗車料金が高いかどうか
+	// 1a乗車料金が残高以内の場合
 	if total-charge >= 0 {
-		// カードポイントに余りがある場合
+		// 2a ICカード内のポイントに余りがある場合
 		if card.Point > 0 {
+			// 3 ポイントを乗車料金分差し引く
 			tmp = card.Point - charge
-			// fmt.Printf("tmp: %d\n", tmp)
-			if tmp <= 0 {
+			// 3a ポイントから差し引いた後、ポイントがマイナスになる場合
+			if tmp < 0 {
 				// print("1\n")
-				card.Point = card.Point - charge
-				card.Point = card.Point - tmp
+				/*
+					card.Point = card.Point - charge
+					card.Point = card.Point - tmp
+				*/
+				// カードポイントを0にする
+				card.Point = 0
+				// カード残高から不足分を減算処理する
 				card.Balance = card.Balance + tmp
 				// fmt.Printf("end card.Point: %d\n", card.Point)
 				// fmt.Printf("end card.Balance: %d\n", card.Balance)
 				return true
+				// 3b ポイントから差し引いた後、ポイントが0以上になる場合
 			} else {
 				// print("2\n")
-				card.Point = card.Point - charge
+				// カードポイントから減算処理する
+				// card.Point = card.Point - charge
+				card.Point = tmp
 				return true
 			}
-			// カードポイント残高0の場合
+			// 2b ICカード内のポイント残高が0の場合
 		} else {
 			card.Balance = card.Balance - charge
 			return true
 		}
 	} else {
-		// そもそも初めから残高不足だからfalse
+		// 1b 乗車料金がICカード残が会場の場合（残高不足の場合）
 		return false
 	}
 }
