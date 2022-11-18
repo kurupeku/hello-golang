2074ae35de9752d70faaabc67a508762e2d949b1
eyes-919 <eyes.works@11069161+eyes-919@users.noreply.github.com>
2022-11-18 01:30:36 +0000

    2022/11/18 code clean up

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -1,8 +1,6 @@
 package chapter04
 
-import (
 // "fmt"
-)
 
 type Card struct {
 	Balance int
@@ -11,13 +9,6 @@ type Card struct {
 
 func Kaisatsu(charge int, card *Card) bool {
 	// TODO: 実装
-
-	/*
-		fmt.Printf("乗車料金:%d\n", charge)
-		fmt.Printf("card.Balance:%d\n", card.Balance)
-		fmt.Printf("card.Point:%d\n", card.Point)
-	*/
-
 	var total int
 	var tmp int
 
@@ -32,23 +23,13 @@ func Kaisatsu(charge int, card *Card) bool {
 			tmp = card.Point - charge
 			// 3a ポイントから差し引いた後、ポイントがマイナスになる場合
 			if tmp < 0 {
-				// print("1\n")
-				/*
-					card.Point = card.Point - charge
-					card.Point = card.Point - tmp
-				*/
 				// カードポイントを0にする
 				card.Point = 0
 				// カード残高から不足分を減算処理する
 				card.Balance = card.Balance + tmp
-				// fmt.Printf("end card.Point: %d\n", card.Point)
-				// fmt.Printf("end card.Balance: %d\n", card.Balance)
 				return true
 				// 3b ポイントから差し引いた後、ポイントが0以上になる場合
 			} else {
-				// print("2\n")
-				// カードポイントから減算処理する
-				// card.Point = card.Point - charge
 				card.Point = tmp
 				return true
 			}
