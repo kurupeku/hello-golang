902ab3a2794fa3532655b82a56029131583abea3
kz-create <mr.kzak315@gmail.com>
2022-11-21 11:16:49 +0000

    fixed Kaisatsu

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -6,6 +6,14 @@ type Card struct {
 }
 
 func Kaisatsu(charge int, card *Card) bool {
-	// TODO: 実装
-	return false
+	if s := card.Balance + card.Point; charge > s {
+		return false
+	}
+
+	card.Point -= charge
+	if card.Point < 0 {
+		card.Balance += card.Point
+		card.Point = 0
+	}
+	return true
 }
