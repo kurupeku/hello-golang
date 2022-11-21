6b4c39465770e44b85e57cff63aa0985ef5b938f
akioh <a.a.10.0326@gmail.com>
2022-11-21 07:21:14 +0000

    スマートな書き方がわからない…

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -7,5 +7,23 @@ type Card struct {
 
 func Kaisatsu(charge int, card *Card) bool {
 	// TODO: 実装
-	return false
+	now_sum_balance_and_point := card.Balance + card.Point
+	num := now_sum_balance_and_point - charge
+	switch {
+	case num < 0:
+		return false
+	default:
+		calc_point := card.Point - charge
+		switch {
+		case calc_point < 0:
+			calc_balance := card.Balance + calc_point
+			card.Balance = calc_balance
+			card.Point = 0
+			return true
+		default:
+			card.Point = calc_point
+			return true
+
+		}
+	}
 }
