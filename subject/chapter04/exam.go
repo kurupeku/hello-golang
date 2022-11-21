a77bcbdb146071255af6b8ef790c6a4086223737
nanchiu9 <takahiro.miyamoto@rakus.co.jp>
2022-11-21 11:01:34 +0000

    ださい書き方

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -7,5 +7,28 @@ type Card struct {
 
 func Kaisatsu(charge int, card *Card) bool {
 	// TODO: 実装
-	return false
+	/*
+		    料金 残高 ポイント
+		    if Point >= 料金
+			Point = Point - 料金
+			else
+			Point = 0
+			Balance = Balance - (料金 - Point)
+		    return true
+	*/
+
+	if card.Balance+card.Point >= charge {
+		if card.Point >= charge {
+			card.Point = card.Point - charge
+			return true
+		} else {
+			card.Balance = card.Balance - (charge - card.Point)
+			card.Point = 0
+			return true
+		}
+
+	} else {
+		return false
+	}
+
 }
