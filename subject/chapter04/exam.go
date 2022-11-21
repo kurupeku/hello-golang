a4c9029ac1489ba1811a713841e0e707d57f1186
tmastut <adeliepinguis@gmail.com>
2022-11-21 11:03:47 +0000

    chapter04 commit

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -7,5 +7,13 @@ type Card struct {
 
 func Kaisatsu(charge int, card *Card) bool {
 	// TODO: 実装
+	if charge <= card.Balance+card.Point {
+		for charge > 0 && card.Point > 0 {
+			card.Point--
+			charge--
+		}
+		card.Balance -= charge
+		return true
+	}
 	return false
 }
