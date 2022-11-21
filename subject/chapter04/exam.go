b9e9ab2bfd352768a2852c6769c6d9e0054f8820
tmastut <adeliepinguis@gmail.com>
2022-11-21 11:10:47 +0000

    chapter04 commit (コメント文追加)

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -7,11 +7,15 @@ type Card struct {
 
 func Kaisatsu(charge int, card *Card) bool {
 	// TODO: 実装
+	// 残高+ポイントが乗車料金より多いかの判定
 	if charge <= card.Balance+card.Point {
+		// 乗車料金が0より大きい かつ ポイントが0より大きい場合にループ
 		for charge > 0 && card.Point > 0 {
-			card.Point--
+			// 乗車料金とポイントをループする度に1マイナスする
 			charge--
+			card.Point--
 		}
+		// ポイントから引けなかった分の乗車料金を残高からマイナスする（ポイントで足りた場合は0を引く）
 		card.Balance -= charge
 		return true
 	}
