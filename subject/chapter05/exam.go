4fbccf7d920e30c68fc682083d309894962c4b23
sakekoba <114719539+sakekoba@users.noreply.github.com>
2022-11-22 11:35:54 +0000

    chapter05 answerd

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,36 @@
 package chapter05
 
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	// 達磨の保護
+	if len(daruma) < 2 {
+		return daruma
+
+	}
+
+	// 真ん中の添え字
+	sprit := (len(daruma) - 1) / 2
+
+	// 新たな配列を作成
+	after_daruma := make([]int, 0, len(daruma)-1)
+	for i, val := range daruma {
+		if i == sprit {
+			continue
+		}
+		after_daruma = append(after_daruma, val)
+	}
+	return after_daruma
 }
 
 func MatrixMultiple(seed []int) [][]int {
 	// TODO: 実装
-	return [][]int{}
+	matrix := make([][]int, 0, len(seed))
+	for i := range seed {
+		table := make([]int, 0, len(seed))
+		for _, val := range seed {
+			table = append(table, seed[i]*val)
+		}
+		matrix = append(matrix, table)
+	}
+
+	return matrix
 }
