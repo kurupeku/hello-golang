163888f901b2cbe4b3c17503c8bfb8c1b846a750
gumamon <110001528+ninaguma3@users.noreply.github.com>
2022-11-25 15:05:45 +0900

    daruma passed.

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,8 +1,25 @@
 package chapter05
 
+//import "fmt"
+
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	target := 0
+
+	switch {
+	case len(daruma) < 2:
+		return daruma
+	case len(daruma) == 2:
+		return daruma[1:]
+	case len(daruma)%2 == 0: //even
+		target = len(daruma)/2 - 1
+	case len(daruma)%2 == 1: //odd
+		target = len(daruma) / 2
+	}
+	//fmt.Printf("daruma-len is %v\n", len(daruma))
+
+	// 第二引数のsliceを ...で要素展開
+	return append(daruma[:target], daruma[target+1:]...)
+
 }
 
 func MatrixMultiple(seed []int) [][]int {
