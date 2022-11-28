5383ad71a7b0d354641cdea64e9f573a06f46254
nanchiu9 <takahiro.miyamoto@rakus.co.jp>
2022-11-28 10:38:26 +0000

    だるまは深い海に落ちた

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,45 @@
 package chapter05
 
+import (
+	"fmt"
+	"math"
+)
+
 func DarumaDrop(daruma []int) []int {
 	// TODO: 実装
-	return []int{}
+	fmt.Println(len(daruma))
+	darumaLen := len(daruma)
+	darumaPosition := int(math.Ceil(float64(darumaLen) / 2))
+
+	if darumaLen <= 1 {
+		return daruma
+	} else {
+		front := daruma[0 : darumaPosition-1]
+		fmt.Println(front)
+		back := daruma[darumaPosition:darumaLen]
+		fmt.Println(back)
+		newDaruma := append(front, back...)
+		return newDaruma
+	}
 }
 
 func MatrixMultiple(seed []int) [][]int {
 	// TODO: 実装
-	return [][]int{}
+	fmt.Println(seed)
+	tree := make([][]int, len(seed))
+	for i, v := range seed {
+		tree[i] = make([]int, len(seed))
+		for j, k := range seed {
+			tree[i][j] = v * k
+			fmt.Println(tree[i][j])
+		}
+
+	}
+	return tree
+	/*
+			0*0 0*1 0*2
+			1*0 1*1 1*2
+		    2*0 2*1 2*2
+	*/
+
 }
