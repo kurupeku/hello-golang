f2d5c538be00616aa99efbacd74a4badfb1f0705
kz-create <mr.kzak315@gmail.com>
2022-11-28 16:17:45 +0000

    fix DarumaDrop & MatrixMultiple

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,40 @@
 package chapter05
 
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	if dl := len(daruma); dl == 0 || dl == 1 {
+		return daruma
+	}
+
+	l := len(daruma) % 2
+	m := len(daruma) / 2
+	if l != 0 {
+		daruma = daruma[:m+copy(daruma[m:], daruma[m+1:])]
+	} else {
+		if daruma[m] > daruma[m-1] {
+			daruma = daruma[:m-1+copy(daruma[m-1:], daruma[m:])]
+		} else {
+			daruma = daruma[:m+copy(daruma[m:], daruma[m+1:])]
+		}
+	}
+
+	return daruma
 }
 
 func MatrixMultiple(seed []int) [][]int {
-	// TODO: 実装
-	return [][]int{}
+	l := len(seed)
+	multiple := make([][]int, l)
+	if l == 0 {
+		return multiple
+	}
+
+	for i := 0; i < l; i++ {
+		multiple[i] = make([]int, l)
+	}
+
+	for i := 0; i < l; i++ {
+		for j := 0; j < l; j++ {
+			multiple[i][j] = seed[i] * seed[j]
+		}
+	}
+	return multiple
 }
