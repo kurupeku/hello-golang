e68136391d0da6371c9cf7b2066a2335d31a0791
kazukiwatanabe21 <kazuki.watanabe@rakus.co.jp>
2022-11-28 11:06:09 +0000

    chapter05 done, yeah.

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,53 @@
 package chapter05
 
+import (
+	"math"
+)
+
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	daruma_len := len(daruma)
+	if daruma_len <= 1 {
+		return daruma
+	}
+
+	if daruma_len == 2 {
+		daruma = daruma[1:]
+		return daruma
+	}
+
+	var first_half, latter_half []int
+
+	if (daruma_len % 2) == 0 {
+		number_of_pulled_out := int(math.Floor(float64(daruma_len / 2)))
+		first_half = daruma[:number_of_pulled_out-1]
+		latter_half = daruma[number_of_pulled_out:]
+	}
+
+	if (daruma_len % 2) == 1 {
+		number_of_pulled_out := int(math.Ceil(float64(daruma_len / 2)))
+		first_half = daruma[:number_of_pulled_out]
+		latter_half = daruma[number_of_pulled_out+1:]
+	}
+
+	daruma = append(first_half, latter_half...)
+	return daruma
 }
 
 func MatrixMultiple(seed []int) [][]int {
-	// TODO: 実装
-	return [][]int{}
+	var answer [][]int
+	var pre_answer []int
+
+	if len(seed) == 0 {
+		return [][]int{}
+	}
+
+	for _, v := range seed {
+		pre_answer = nil
+		for _, x := range seed {
+			pre_answer = append(pre_answer, v*x)
+		}
+		answer = append(answer, pre_answer)
+	}
+	return answer
+
 }
