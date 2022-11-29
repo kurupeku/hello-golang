8c4976939c642d70d2d0e63ea2e12ac69802dce3
chmikata <chmikata@gmail.com>
2022-11-29 11:36:57 +0000

    complete

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,25 @@
 package chapter05
 
+import (
+	"math"
+)
+
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	if len(daruma) <= 1 {
+		return daruma
+	}
+	target := int(math.Ceil(float64(len(daruma))/2)) - 1
+	return append(daruma[0:target], daruma[target+1:]...)
 }
 
 func MatrixMultiple(seed []int) [][]int {
-	// TODO: 実装
-	return [][]int{}
+	col := make([][]int, len(seed))
+	for i := range seed {
+		row := make([]int, len(seed))
+		for j := range seed {
+			row[j] = seed[i] * seed[j]
+		}
+		col[i] = row
+	}
+	return col
 }
