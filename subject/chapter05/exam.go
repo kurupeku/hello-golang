30d1b807142f6f4e40733bf70873af69f894d4d8
daigo1130 <daigo1130@gmail.com>
2022-11-29 02:40:47 +0000

    Fixed:chapter05

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,56 @@
 package chapter05
 
+//import "math"
+
 func DarumaDrop(daruma []int) []int {
 	// TODO: 実装
-	return []int{}
+
+	daruma2 := []int{}
+	l := len(daruma)
+	m := l / 2
+
+	if l <= 1 {
+		return daruma
+	}
+
+	if l%2 == 0 {
+		if daruma[m-1] < daruma[m] {
+			for _, v := range daruma {
+				if v != daruma[m-1] {
+					daruma2 = append(daruma2, v)
+				}
+			}
+		} else {
+			for _, v := range daruma {
+				if v != daruma[m] {
+					daruma2 = append(daruma2, v)
+				}
+			}
+		}
+
+	} else {
+		for _, v := range daruma {
+			if v != daruma[m] {
+				daruma2 = append(daruma2, v)
+			}
+		}
+	}
+
+	return daruma2
 }
 
 func MatrixMultiple(seed []int) [][]int {
 	// TODO: 実装
-	return [][]int{}
+
+	matrix := [][]int{}
+
+	for i := range seed {
+		matrix2 := []int{}
+		for j := range seed {
+			matrix2 = append(matrix2, seed[i]*seed[j])
+		}
+		matrix = append(matrix, matrix2)
+	}
+
+	return matrix
 }
