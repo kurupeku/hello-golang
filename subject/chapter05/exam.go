b23d184c22db6196769799ee0bc0e643913a28b4
kanzakiyotaro <yotaro.kanzaki0729@gmail.com>
2022-11-29 16:14:27 +0900

    chapter5

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,42 @@
 package chapter05
 
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	// TODO: 実装（無理やり）
+	darumaResult := daruma
+	darumaLen := len(daruma)
+	var length int
+	if darumaLen < 2 {
+		return daruma
+	} else if darumaLen%2 == 0 {
+		length = darumaLen / 2
+
+		if daruma[length] > daruma[length-1] {
+			length -= 1
+		}
+
+	} else if darumaLen%2 == 1 {
+		length = darumaLen / 2
+	}
+
+	if length == 0 {
+		darumaResult = daruma[length+1:]
+	} else {
+		darumaResult = append(daruma[:length], daruma[length+1:]...)
+	}
+
+	return darumaResult
 }
 
 func MatrixMultiple(seed []int) [][]int {
-	// TODO: 実装
-	return [][]int{}
+	// TODO: 実装(王道)
+	matrix := make([][]int, 0, len(seed))
+	for i := range seed {
+		table := make([]int, 0, len(seed))
+		for _, val := range seed {
+			table = append(table, seed[i]*val)
+		}
+		matrix = append(matrix, table)
+	}
+
+	return matrix
 }
