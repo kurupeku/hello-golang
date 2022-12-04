01db6a5c241d34af555af1dd5fae9f009b1425fd
chmikata <chmikata@gmail.com>
2022-12-04 05:12:10 +0000

    complete

--- subject/chapter07/exam.go
+++ subject/chapter07/exam.go
@@ -1,7 +1,9 @@
 package chapter07
 
 import (
+	"fmt"
 	"sort"
+	"strings"
 )
 
 type Item struct {
@@ -26,10 +28,20 @@ func (c *Casher) itemNames() []string {
 }
 
 func (c *Casher) Purchase(item *Item) {
-	// TODO: 実装
+	if c.List == nil {
+		c.List = make(map[string]int, 0)
+	}
+	c.List[item.Name] += item.Price
+	c.TotalPrice += item.Price
 }
 
 func (c *Casher) Receipt() string {
-	// TODO: 実装
-	return ""
+	var sb strings.Builder
+	sb.WriteString("\nラーメン道 楽酢\n\n")
+	for _, val := range c.itemNames() {
+		sb.WriteString(fmt.Sprintf("%-10s:%9d\n", val, c.List[val]))
+	}
+	sb.WriteString("--------------------\n")
+	sb.WriteString(fmt.Sprintf("%20d\n", c.TotalPrice))
+	return sb.String()
 }
