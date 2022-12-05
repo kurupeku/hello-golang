e31002b9c61de9bb1bedf0f9a2568bcafeeb1dfa
kazukiwatanabe21 <kazuki.watanabe@rakus.co.jp>
2022-12-05 10:29:12 +0000

    chapter 06 done, yeah !!

--- subject/chapter06/exam.go
+++ subject/chapter06/exam.go
@@ -2,6 +2,7 @@ package chapter06
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/kurupeku/hello-golang/helper"
 )
@@ -17,11 +18,26 @@ func (n AhoNumber) aho() string {
 }
 
 func (n AhoNumber) Call() string {
-	// TODO: 実装
-	return ""
+	Response := n.String()
+
+	if strings.Contains(Response, "3") {
+		Response = helper.AhoString(int(n))
+		return Response
+	}
+
+	if n%3 == 0 {
+		Response = helper.AhoString(int(n))
+		return Response
+	}
+	return Response
 }
 
 func Nabeatsu(n int) []string {
-	// TODO: 実装
-	return []string{}
+	var answer []string = nil
+	for i := 1; i <= n; i++ {
+		m := AhoNumber(i)
+		var pre_answer string = AhoNumber.Call(m)
+		answer = append(answer, pre_answer)
+	}
+	return answer
 }
