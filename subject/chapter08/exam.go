2600a8c216198e455f8bb36d9f045564d184b345
chmikata <chmikata@gmail.com>
2022-12-06 10:21:07 +0000

    complete

--- subject/chapter08/exam.go
+++ subject/chapter08/exam.go
@@ -1,5 +1,7 @@
 package chapter08
 
+import "math"
+
 type Car interface {
 	PricePer15Minutes() int
 	MaxPrice() int
@@ -7,11 +9,39 @@ type Car interface {
 
 type Basic struct{}
 
+func (b Basic) PricePer15Minutes() int {
+	return 220
+}
+
+func (b Basic) MaxPrice() int {
+	return 4290
+}
+
 type Middle struct{}
+
+func (m Middle) PricePer15Minutes() int {
+	return 330
+}
+
+func (m Middle) MaxPrice() int {
+	return 6490
+}
 
 type Premium struct{}
 
+func (p Premium) PricePer15Minutes() int {
+	return 440
+}
+
+func (p Premium) MaxPrice() int {
+	return 8690
+}
+
 func Calc(car Car, minutes int) int {
-	// TODO: 実装
-	return 0
+	maxcntPrice := minutes / 360 * car.MaxPrice()
+	mincntPrice := int(math.Ceil(float64(minutes%360)/15)) * car.PricePer15Minutes()
+	if mincntPrice > car.MaxPrice() {
+		mincntPrice = car.MaxPrice()
+	}
+	return maxcntPrice + mincntPrice
 }
