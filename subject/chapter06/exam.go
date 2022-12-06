86894d024f22668d1b11d5aa09101608c9910e03
tatsuromiyazaki <tatsuro.miyazaki@rakus.co.jp>
2022-12-06 19:06:22 +0900

    chapter06

--- subject/chapter06/exam.go
+++ subject/chapter06/exam.go
@@ -4,6 +4,8 @@ import (
 	"strconv"
 
 	"github.com/kurupeku/hello-golang/helper"
+
+	"strings"
 )
 
 type AhoNumber int
@@ -16,12 +18,36 @@ func (n AhoNumber) aho() string {
 	return helper.AhoString(int(n))
 }
 
+func (n AhoNumber) judgeLetter() bool {
+
+	if strings.Contains(n.String(), "3") {
+		return true
+	} else if n%3 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
 func (n AhoNumber) Call() string {
 	// TODO: 実装
-	return ""
+
+	if n.judgeLetter() {
+		return n.aho()
+	}
+
+	return n.String()
 }
 
 func Nabeatsu(n int) []string {
 	// TODO: 実装
-	return []string{}
+
+	var answer []string
+
+	for i := 1; i <= n; i++ {
+		aho := AhoNumber(i)
+		answer = append(answer, aho.Call())
+	}
+
+	return answer
 }
