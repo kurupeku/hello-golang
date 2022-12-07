257a72b7f7574fbfd2c420991dce542d8bfb9fe0
ryotaro-rks <84486303+ryotaro-rks@users.noreply.github.com>
2022-12-07 18:06:43 +0900

    chapter5

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,50 @@
 package chapter05
 
+func getDropDaruma(dropNum int, daruma []int) []int {
+	var ans []int;
+	for i, v := range daruma {
+		if (i != dropNum) {
+			ans = append(ans, v)
+		}
+	}
+	return ans;
+}
+
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	darumaLen := len(daruma);
+
+	if (darumaLen == 0) {
+		return []int{};
+	}
+
+	if (darumaLen == 1) {
+		return daruma;
+	}
+
+	if (darumaLen % 2 == 1) {
+		dropNum := int(darumaLen/2)
+		return getDropDaruma(dropNum, daruma);
+	} else {
+		dropNum := darumaLen/2 - 1
+		return getDropDaruma(dropNum, daruma);
+	}
 }
 
 func MatrixMultiple(seed []int) [][]int {
-	// TODO: 実装
-	return [][]int{}
+	seedLen := len(seed)
+
+	if (seedLen == 0) {
+		return [][]int{}
+	}
+
+	var ans [][]int
+	for _, v := range seed {
+		var column []int
+		for j := 0; j < seedLen; j++ {
+			column = append(column, v * seed[j])
+		}
+		ans = append(ans, column)
+	}
+
+	return ans
 }
