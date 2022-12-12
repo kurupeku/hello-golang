09796c5bc03ee94660fb02c1fd524165c0b47d78
ryotaro-rks <84486303+ryotaro-rks@users.noreply.github.com>
2022-12-12 16:06:14 +0900

    chapter07

--- subject/chapter07/exam.go
+++ subject/chapter07/exam.go
@@ -2,6 +2,9 @@ package chapter07
 
 import (
 	"sort"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Item struct {
@@ -26,10 +29,70 @@ func (c *Casher) itemNames() []string {
 }
 
 func (c *Casher) Purchase(item *Item) {
-	// TODO: 実装
+	if c.List == nil {
+		c.List = make(map[string]int)
+	}
+
+	_, exist := c.List[item.Name];
+	if exist {
+		c.List[item.Name] += item.Price;
+	} else {
+		c.List[item.Name] = item.Price;
+	}
+
+	c.TotalPrice += item.Price;
+}
+
+func getTitle() string {
+	title := "\n"
+	title += fmt.Sprintf("%s\n", "ラーメン道 楽酢") 
+	title += "\n"
+	return title;
+}
+
+func getItemNameAndPrice(c *Casher) string {
+	var itemNameAndPrice string
+
+	MaxItemNameLength := 10
+	MaxPriceLength := 9
+	
+	for _, itemName := range c.itemNames() {
+		// 商品名を追加
+		itemNameAndPrice += itemName
+
+		// 商品名の後ろに半角スペースを追加
+		itemNameAndPrice += strings.Repeat(" ", MaxItemNameLength - len(itemName))
+
+		// :による区切り
+		itemNameAndPrice += ":"
+
+		// 商品価格の前に半角スペースを追加
+		ItemPriceLength := len(strconv.Itoa(c.List[itemName]))
+		itemNameAndPrice += strings.Repeat(" ", MaxPriceLength - ItemPriceLength)
+
+		// 商品価格を追加
+		itemNameAndPrice += strconv.Itoa(c.List[itemName])
+
+		itemNameAndPrice += "\n"
+	}
+
+	return itemNameAndPrice;
+}
+
+func getTotalPrice(c *Casher) string {
+	var totalPrice string;
+	MaxLineLength := 20
+	TotalPriceLength := len(strconv.Itoa(c.TotalPrice))
+	totalPrice += strings.Repeat(" ", MaxLineLength - TotalPriceLength)
+	totalPrice += strconv.Itoa(c.TotalPrice)
+	totalPrice += "\n"
+	return totalPrice;
 }
 
 func (c *Casher) Receipt() string {
-	// TODO: 実装
-	return ""
+	ans := getTitle()
+	ans += getItemNameAndPrice(c)
+	ans += "--------------------\n"
+	ans += getTotalPrice(c)
+	return ans
 }
