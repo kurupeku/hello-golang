0ca35ce7767d2104c19e9cfcbc2994eb7dae2bd3
ryotaro-rks <84486303+ryotaro-rks@users.noreply.github.com>
2022-12-13 19:40:56 +0900

    chapter08

--- subject/chapter08/exam.go
+++ subject/chapter08/exam.go
@@ -7,11 +7,51 @@ type Car interface {
 
 type Basic struct{}
 
+func (basic Basic) PricePer15Minutes() int {
+	return 220;
+}
+
+func (basic Basic) MaxPrice() int {
+	return 4290;
+}
+
 type Middle struct{}
+
+func (middle Middle) PricePer15Minutes() int {
+	return 330;
+}
+
+func (middle Middle) MaxPrice() int {
+	return 6490;
+}
 
 type Premium struct{}
 
+func (premium Premium) PricePer15Minutes() int {
+	return 440;
+}
+
+func (premium Premium) MaxPrice() int {
+	return 8690;
+}
+
+func getTotalPricePer15(pricePer15Minutes int, minutes int) int {
+	return (minutes / 15 + 1) * pricePer15Minutes
+}
+
+func min(a int, b int) int {
+	if a > b {
+		return b
+	} else {
+		return a
+	}
+}
+
 func Calc(car Car, minutes int) int {
-	// TODO: 実装
-	return 0
+	hour6 := 360
+	countOver6hour := minutes/hour6
+	pricePerMaxPrice := car.MaxPrice() * countOver6hour
+	remainingPrice := getTotalPricePer15(car.PricePer15Minutes(), (minutes - hour6 * countOver6hour))
+	price := pricePerMaxPrice + min(remainingPrice, car.MaxPrice())
+	return price
 }
