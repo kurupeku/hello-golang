9153d99e549814bdcd5692efc777c82725dc4eeb
sakekoba <114719539+sakekoba@users.noreply.github.com>
2022-12-13 10:58:55 +0000

    Answerd Chapter08

--- subject/chapter08/exam.go
+++ subject/chapter08/exam.go
@@ -11,7 +11,36 @@ type Middle struct{}
 
 type Premium struct{}
 
+func (basic Basic) PricePer15Minutes() int {
+	return 220
+}
+func (middle Middle) PricePer15Minutes() int {
+	return 330
+}
+func (premium Premium) PricePer15Minutes() int {
+	return 440
+}
+
+func (basic Basic) MaxPrice() int {
+	return 4290
+}
+func (middle Middle) MaxPrice() int {
+	return 6490
+}
+func (premium Premium) MaxPrice() int {
+	return 8690
+}
+
 func Calc(car Car, minutes int) int {
 	// TODO: 実装
-	return 0
+	max_price_times := minutes / 360
+	remain_minutes := minutes % 360
+	price := car.MaxPrice() * max_price_times
+	price_times := remain_minutes/15 + 1
+	remain_price := car.PricePer15Minutes() * price_times
+	if remain_price > car.MaxPrice() {
+		remain_price = car.MaxPrice()
+	}
+	price += remain_price
+	return price
 }
