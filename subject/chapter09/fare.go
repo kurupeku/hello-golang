8562ee59eb0d1cf94577e56020a14a5bd90e8339
gumamon <110001528+ninaguma3@users.noreply.github.com>
2022-12-20 20:48:22 +0900

    暫定 fare.go

--- subject/chapter09/fare.go
+++ subject/chapter09/fare.go
@@ -1,6 +1,6 @@
 package chapter09
 
-//import "github.com/kurupeku/hello-golang/helper"
+import "github.com/kurupeku/hello-golang/helper"
 
 type Fare struct {
 	From string
@@ -8,26 +8,103 @@ type Fare struct {
 }
 
 func (f *Fare) innerDistance() int {
-	// TODO: 内回りの距離を返すように実装
-	return 0
+	// 内回りの距離を返すように実装
+	distance := 0
+	currentStation := f.From
+	for {
+		if currentStation == f.To {
+			break
+		}
+		currentStation = helper.InnerNextStation(currentStation)
+		distance = distance + helper.InnerLoopDistance(currentStation)
+
+	}
+	return distance
 }
 
 func (f *Fare) outerDistance() int {
-	// TODO: 外回りの距離を返すように実装
-	return 0
+	// 外回りの距離を返すように実装
+	distance := 0
+	currentStation := f.From
+	for {
+		if currentStation == f.To {
+			break
+		}
+		currentStation = helper.OuterNextStation(currentStation)
+		distance = distance + helper.OuterLoopDistance(currentStation)
+	}
+
+	return distance
 }
 
 func (f *Fare) distance() int {
 	// 内回りと外回りの距離を比較し、短い方を返すように実装
-	return 0
+	distance := 0
+
+	switch {
+	case f.innerDistance() < f.outerDistance():
+		distance = f.outerDistance()
+	default:
+		distance = f.innerDistance()
+	}
+
+	return distance
 }
 
 func (f *Fare) TicketCharge() int {
-	// TODO: 切符の料金を返すように実装
-	return 0
+	// 切符の料金を返すように実装
+	distance := f.distance()
+	charge := 0
+
+	switch {
+	case distance == 0:
+		charge = 0
+	case distance < 4000:
+		charge = 140
+	case distance < 7000:
+		charge = 160
+	case distance < 11000:
+		charge = 170
+	case distance < 16000:
+		charge = 200
+	case distance < 21000:
+		charge = 270
+	case distance < 26000:
+		charge = 350
+	case distance < 31000:
+		charge = 420
+	default:
+		charge = 490
+	}
+
+	return charge
 }
 
 func (f *Fare) CardCharge() int {
-	// TODO: IC カードの料金を返すように実装
-	return 0
+	// 切符の料金を返すように実装
+	distance := f.distance()
+	charge := 0
+
+	switch {
+	case distance == 0:
+		charge = 0
+	case distance < 4000:
+		charge = 136
+	case distance < 7000:
+		charge = 157
+	case distance < 11000:
+		charge = 168
+	case distance < 16000:
+		charge = 198
+	case distance < 21000:
+		charge = 264
+	case distance < 26000:
+		charge = 341
+	case distance < 31000:
+		charge = 418
+	default:
+		charge = 484
+	}
+
+	return charge
 }
