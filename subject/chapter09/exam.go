133a2a204f0e5d398803b292876b7deb4206842f
gumamon <110001528+ninaguma3@users.noreply.github.com>
2022-12-20 21:10:30 +0900

    exam.go 作りかけ

--- subject/chapter09/exam.go
+++ subject/chapter09/exam.go
@@ -1,6 +1,26 @@
 package chapter09
 
+import "fmt"
+
 func Kaisatsu(from, to string, charger Charger) (bool, error) {
 	// TODO: 実装
+
+	// Ticket or Card
+	var i interface{} = charger
+
+	switch v := i.(type) {
+	case *Ticket:
+		fmt.Printf("Type is Ticket = %T", v)
+		// 距離を確認
+
+		// 利用料金を算出
+	case *Card:
+		fmt.Printf("Type is Card = %T", v)
+	default:
+		fmt.Printf("Type is Unknown = %T", v)
+	}
+
+	// 利用料金(charge)を算出
+
 	return false, nil
 }
