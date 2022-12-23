928fcd58cfed4afaad2b5ff607e612045d7e792b
iino-tmk <tomoko.iino@rakus.co.jp>
2022-12-23 09:57:22 +0900

    chapter05

--- subject/chapter05/exam.go
+++ subject/chapter05/exam.go
@@ -1,11 +1,30 @@
 package chapter05
 
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	darumaLength := len(daruma)
+	if darumaLength == 0 || darumaLength == 1 {
+		return daruma
+	}
+
+	sliceNum := darumaLength / 2
+	if darumaLength%2 == 0 {
+		return append(daruma[0:sliceNum-1], daruma[sliceNum:]...)
+	} else {
+		return append(daruma[0:sliceNum], daruma[sliceNum+1:]...)
+	}
 }
 
 func MatrixMultiple(seed []int) [][]int {
-	// TODO: 実装
-	return [][]int{}
+	seedLength := len(seed)
+	multiSlice := make([][]int, seedLength)
+
+	for i, value := range seed {
+		slice := make([]int, seedLength)
+		for j := 0; j < seedLength; j++ {
+			slice[j] = value * seed[j]
+		}
+		multiSlice[i] = slice
+	}
+
+	return multiSlice
 }
