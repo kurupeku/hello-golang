d80082b02b753a8226fd5685c5c4491d8be563e1
daigo1130 <daigo1130@gmail.com>
2022-12-23 04:39:49 +0000

    Fixed:chapter09

--- subject/chapter09/exam.go
+++ subject/chapter09/exam.go
@@ -1,6 +1,37 @@
 package chapter09
 
+import (
+	"errors"
+)
+
 func Kaisatsu(from, to string, charger Charger) (bool, error) {
 	// TODO: 実装
-	return false, nil
+
+	fare := Fare{from, to}
+	var charge int
+
+	switch charger.(type) {
+	case *Ticket:
+		if fare.TicketCharge() == 0 {
+			return false, errors.New("TicketCharge is 0")
+		}
+		charge = fare.TicketCharge()
+
+	case *Card:
+		if fare.CardCharge() == 0 {
+			return false, errors.New("CardCharge is 0")
+		}
+		charge = fare.CardCharge()
+
+	default:
+		return false, errors.New("unexpected error")
+	}
+
+	if charger.Amount() < charge {
+		return false, nil
+	}
+
+	charger.Use(charge)
+
+	return true, nil
 }
--- subject/chapter09/fare.go
+++ subject/chapter09/fare.go
@@ -8,26 +8,107 @@ type Fare struct {
 }
 
 func (f *Fare) innerDistance() int {
-	// TODO: 内回りの料金を返すように実装
-	return 0
+	// TODO: 内回りの距離を返すように実装
+
+	current_station := f.From
+	distance := 0
+
+	if f.From == f.To {
+		return 0
+	}
+
+	for {
+		next := helper.InnerNextStation(current_station)
+		distance += helper.InnerLoopDistance(next)
+		current_station = next
+
+		if current_station == f.To {
+			break
+		}
+	}
+
+	return distance
 }
 
 func (f *Fare) outerDistance() int {
 	// TODO: 外回りの距離を返すように実装
-	return 0
+
+	current_station := f.From
+	distance := 0
+
+	if f.From == f.To {
+		return 0
+	}
+
+	for {
+		next := helper.OuterNextStation(current_station)
+		distance += helper.OuterLoopDistance(next)
+		current_station = next
+
+		if current_station == f.To {
+			break
+		}
+	}
+
+	return distance
 }
 
 func (f *Fare) distance() int {
 	// 内回りと外回りの距離を比較し、短い方を返すように実装
-	return 0
+
+	if f.innerDistance() > f.outerDistance() {
+		return f.outerDistance()
+	} else {
+		return f.innerDistance()
+	}
 }
 
 func (f *Fare) TicketCharge() int {
 	// TODO: 切符の料金を返すように実装
-	return 0
+
+	if f.distance() == 0 {
+		return 0
+	} else if f.distance() <= 3999 {
+		return 140
+	} else if f.distance() <= 6999 {
+		return 160
+	} else if f.distance() <= 10999 {
+		return 170
+	} else if f.distance() <= 15999 {
+		return 200
+	} else if f.distance() <= 20999 {
+		return 270
+	} else if f.distance() <= 25999 {
+		return 350
+	} else if f.distance() <= 30999 {
+		return 420
+	} else {
+		return 490
+	}
+
 }
 
 func (f *Fare) CardCharge() int {
 	// TODO: IC カードの料金を返すように実装
-	return 0
+
+	if f.distance() == 0 {
+		return 0
+	} else if f.distance() <= 3999 {
+		return 136
+	} else if f.distance() <= 6999 {
+		return 157
+	} else if f.distance() <= 10999 {
+		return 168
+	} else if f.distance() <= 15999 {
+		return 198
+	} else if f.distance() <= 20999 {
+		return 264
+	} else if f.distance() <= 25999 {
+		return 341
+	} else if f.distance() <= 30999 {
+		return 418
+	} else {
+		return 484
+	}
+
 }
