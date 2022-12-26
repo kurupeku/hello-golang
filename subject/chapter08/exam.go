f52efd8347ced12a036de4b72c64d1100b96495a
kz-create <mr.kzak315@gmail.com>
2022-12-26 08:42:21 +0000

    fix Calc function

--- subject/chapter08/exam.go
+++ subject/chapter08/exam.go
@@ -1,5 +1,9 @@
 package chapter08
 
+import (
+	"math"
+)
+
 type Car interface {
 	PricePer15Minutes() int
 	MaxPrice() int
@@ -11,7 +15,49 @@ type Middle struct{}
 
 type Premium struct{}
 
+func (b *Basic) PricePer15Minutes() int {
+	return 220
+}
+
+func (b *Basic) MaxPrice() int {
+	return 4290
+}
+
+func (m *Middle) PricePer15Minutes() int {
+	return 330
+}
+
+func (m *Middle) MaxPrice() int {
+	return 6490
+}
+
+func (p *Premium) PricePer15Minutes() int {
+	return 440
+}
+
+func (p *Premium) MaxPrice() int {
+	return 8690
+}
+
 func Calc(car Car, minutes int) int {
-	// TODO: 実装
-	return 0
+	MaxMin := 360
+	MaxPrice := car.MaxPrice()
+	fee := 0
+
+	if minutes < MaxMin {
+		fee = car.PricePer15Minutes() * int(math.Ceil(float64(minutes)/15.0))
+		if fee > MaxPrice {
+			fee = MaxPrice
+		}
+	} else {
+		m := minutes - MaxMin
+		if m == 0 {
+			fee = MaxPrice
+		} else {
+			fee = MaxPrice
+			fee += Calc(car, m)
+		}
+	}
+
+	return fee
 }
