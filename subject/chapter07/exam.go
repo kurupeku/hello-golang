5c6fe1c88f4f765fea78ad70268c7267c8bf383b
kz-create <mr.kzak315@gmail.com>
2022-12-26 07:35:57 +0000

    fix Purchase and Receipt functions

--- subject/chapter07/exam.go
+++ subject/chapter07/exam.go
@@ -1,7 +1,9 @@
 package chapter07
 
 import (
+	"fmt"
 	"sort"
+	"strconv"
 )
 
 type Item struct {
@@ -26,10 +28,33 @@ func (c *Casher) itemNames() []string {
 }
 
 func (c *Casher) Purchase(item *Item) {
-	// TODO: 実装
+	arr := make(map[string]int, 0)
+	for k, v := range c.List {
+		arr[k] = v
+	}
+	c.List = make(map[string]int, 0)
+	for k, v := range arr {
+		c.List[k] = v
+	}
+
+	if k, ok := c.List[item.Name]; !ok {
+		c.List[item.Name] = item.Price
+	} else {
+		c.List[item.Name] = k + item.Price
+	}
+	c.TotalPrice += item.Price
 }
 
 func (c *Casher) Receipt() string {
-	// TODO: 実装
-	return ""
+	keys := c.itemNames()
+	r := "\nラーメン道 楽酢\n"
+	r += "\n"
+
+	for _, v := range keys {
+		r += fmt.Sprintf("%-10s:%9s\n", v, strconv.Itoa(c.List[v]))
+	}
+
+	r += "--------------------\n"
+	r += fmt.Sprintf("%20s\n", strconv.Itoa(c.TotalPrice))
+	return r
 }
