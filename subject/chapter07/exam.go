90b7f20847ed18bd56d777ca6f3384c3083ec765
kazukiwatanabe21 <kazuki.watanabe@rakus.co.jp>
2022-12-27 09:26:02 +0000

    chapter07 done , yeah!!

--- subject/chapter07/exam.go
+++ subject/chapter07/exam.go
@@ -1,7 +1,9 @@
 package chapter07
 
 import (
+	"fmt"
 	"sort"
+	"strings"
 )
 
 type Item struct {
@@ -26,10 +28,25 @@ func (c *Casher) itemNames() []string {
 }
 
 func (c *Casher) Purchase(item *Item) {
-	// TODO: 実装
+	if c.List == nil {
+		c.List = make(map[string]int)
+	}
+	c.List[item.Name] += item.Price
+	c.TotalPrice += item.Price
 }
 
 func (c *Casher) Receipt() string {
-	// TODO: 実装
-	return ""
+	store_name := "\nラーメン道 楽酢\n"
+	separator := "\n" + strings.Repeat("-", 20) + "\n"
+	pre_result := make([]string, 0)
+	for _, b := range c.itemNames() {
+		tmp_result := "\n" + fmt.Sprintf("%-10s", b) + ":" + fmt.Sprintf("%9d", c.List[b])
+		pre_result = append(pre_result, tmp_result)
+	}
+	var result string
+	for _, d := range pre_result {
+		result += d
+	}
+
+	return store_name + result + separator + fmt.Sprintf("%20d", c.TotalPrice) + "\n"
 }
