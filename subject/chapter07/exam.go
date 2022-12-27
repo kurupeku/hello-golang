c407f17d8466a6dbedb7aeaeaac5fcad4a19eed7
Yoshinori Ohminato <yoshinori.ominato@rakus.co.jp>
2022-12-27 18:48:02 +0900

    チャプター７

--- subject/chapter07/exam.go
+++ subject/chapter07/exam.go
@@ -1,7 +1,9 @@
 package chapter07
 
 import (
+	"fmt"
 	"sort"
+	"strings"
 )
 
 type Item struct {
@@ -26,10 +28,30 @@ func (c *Casher) itemNames() []string {
 }
 
 func (c *Casher) Purchase(item *Item) {
-	// TODO: 実装
+
+	// 初期化されていなければ初期化
+	if c.List == nil {
+		c.List = map[string]int{}
+	}
+
+	// 受け取ったアイテムを加算
+	c.List[item.Name] += item.Price
+	c.TotalPrice += item.Price
 }
 
 func (c *Casher) Receipt() string {
+
 	// TODO: 実装
-	return ""
+	// ヘッダ部分
+	receipt_str := "\nラーメン道 楽酢\n\n"
+
+	// 品目
+	for _, key := range c.itemNames() {
+		receipt_str += fmt.Sprintf("%-10s:%9d\n", key, c.List[key])
+	}
+	// 合計値
+	receipt_str += strings.Repeat("-", 20) + "\n" +
+		fmt.Sprintf("%20d\n", c.TotalPrice)
+
+	return receipt_str
 }
