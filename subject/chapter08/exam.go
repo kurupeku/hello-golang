d3499b7bff23745dea4413c8690424215bdd5623
kazukiwatanabe21 <kazuki.watanabe@rakus.co.jp>
2022-12-28 00:34:45 +0000

    chapter08 done, yeah!!

--- subject/chapter08/exam.go
+++ subject/chapter08/exam.go
@@ -5,13 +5,58 @@ type Car interface {
 	MaxPrice() int
 }
 
-type Basic struct{}
+type Basic struct {
+	X int
+	Y int
+}
 
-type Middle struct{}
+func (b *Basic) PricePer15Minutes() int {
+	return 220
+}
 
-type Premium struct{}
+func (b *Basic) MaxPrice() int {
+	return 4290
+}
+
+type Middle struct {
+	X int
+	Y int
+}
+
+func (m *Middle) PricePer15Minutes() int {
+	return 330
+}
+
+func (m *Middle) MaxPrice() int {
+	return 6490
+}
+
+type Premium struct {
+	X int
+	Y int
+}
+
+func (p *Premium) PricePer15Minutes() int {
+	return 440
+}
+
+func (p *Premium) MaxPrice() int {
+	return 8690
+}
 
 func Calc(car Car, minutes int) int {
-	// TODO: 実装
-	return 0
+	price := 0
+	count_6hour := minutes / 360
+	amari_6hour := minutes % 360
+	if count_6hour > 0 {
+		price += car.MaxPrice() * count_6hour
+	}
+	count_15minute := amari_6hour / 15
+	tuika_price := car.PricePer15Minutes() * (count_15minute + 1)
+	if tuika_price > car.MaxPrice() {
+		tuika_price = car.MaxPrice()
+	}
+	price += tuika_price
+
+	return price
 }
