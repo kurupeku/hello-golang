b08d90b75aa058ec74050971510b1b9417de94e4
iino-tmk <tomoko.iino@rakus.co.jp>
2023-01-06 13:13:30 +0900

    chapter07

--- subject/chapter07/exam.go
+++ subject/chapter07/exam.go
@@ -1,7 +1,10 @@
 package chapter07
 
 import (
+	"fmt"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 type Item struct {
@@ -13,6 +16,10 @@ type Casher struct {
 	List       map[string]int
 	TotalPrice int
 }
+
+const Newline = "\n"
+const Shopname = "ラーメン道 楽酢"
+const Line = "-"
 
 // c.List の Key を順番固定で返すメソッド
 func (c *Casher) itemNames() []string {
@@ -26,10 +33,32 @@ func (c *Casher) itemNames() []string {
 }
 
 func (c *Casher) Purchase(item *Item) {
-	// TODO: 実装
+	if c.List == nil {
+		c.List = make(map[string]int)
+	}
+
+	c.List[item.Name] += item.Price
+	c.TotalPrice += item.Price
+
+	c.Receipt()
 }
 
 func (c *Casher) Receipt() string {
-	// TODO: 実装
-	return ""
+	receipt := Newline
+	receipt += Shopname
+	receipt += strings.Repeat(Newline, 2)
+
+	for _, value := range c.itemNames() {
+		receipt += fmt.Sprintf("%-10s", value)
+		receipt += ":"
+		receipt += fmt.Sprintf("%9s", strconv.Itoa(c.List[value]))
+		receipt += Newline
+	}
+
+	receipt += strings.Repeat(Line, 20)
+	receipt += Newline
+	receipt += fmt.Sprintf("%20s", strconv.Itoa(c.TotalPrice))
+	receipt += Newline
+
+	return receipt
 }
