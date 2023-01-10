92e081258ee0bc61aa2d4af9c59c9e01ef9d5ec0
kanzakiyotaro <yotaro.kanzaki0729@gmail.com>
2023-01-10 19:35:02 +0900

    chapter06

--- subject/chapter06/exam.go
+++ subject/chapter06/exam.go
@@ -2,11 +2,14 @@ package chapter06
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/kurupeku/hello-golang/helper"
 )
 
 type AhoNumber int
+
+const nabeNum int = 3
 
 func (n AhoNumber) String() string {
 	return strconv.Itoa(int(n))
@@ -17,11 +20,24 @@ func (n AhoNumber) aho() string {
 }
 
 func (n AhoNumber) Call() string {
-	// TODO: 実装
-	return ""
+
+	if (int(n) % nabeNum) == 0 {
+		return n.aho()
+	} else if strings.Contains(n.String(), strconv.Itoa(nabeNum)) {
+		return n.aho()
+	} else {
+		return n.String()
+	}
 }
 
 func Nabeatsu(n int) []string {
+
+	numAry := make([]string, n)
+
+	for countNum := AhoNumber(1); int(countNum) <= n; countNum++ {
+		numAry[countNum-1] = countNum.Call()
+	}
+
 	// TODO: 実装
-	return []string{}
+	return numAry
 }
