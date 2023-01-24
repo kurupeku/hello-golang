edb09dc5f47a5621dc9f628fa0258ea1506d59e2
kanzakiyotaro <yotaro.kanzaki0729@gmail.com>
2023-01-24 17:37:09 +0900

    人のを参考にしながら書いてますが、結局それをみて自分のコードを見直すとそっちのほうがいいとなり修正する羽目に

--- subject/chapter08/exam.go
+++ subject/chapter08/exam.go
@@ -1,5 +1,7 @@
 package chapter08
 
+import "math"
+
 type Car interface {
 	PricePer15Minutes() int
 	MaxPrice() int
@@ -11,7 +13,25 @@ type Middle struct{}
 
 type Premium struct{}
 
+func (c Basic) PricePer15Minutes() int { return 220 }
+
+func (c Basic) MaxPrice() int { return 4290 }
+
+func (c Middle) PricePer15Minutes() int { return 330 }
+
+func (c Middle) MaxPrice() int { return 6490 }
+
+func (c Premium) PricePer15Minutes() int { return 440 }
+
+func (c Premium) MaxPrice() int { return 8690 }
+
 func Calc(car Car, minutes int) int {
-	// TODO: 実装
-	return 0
+	payPerUsePrice := int(math.Ceil(float64(minutes%360)/15)) * car.PricePer15Minutes()
+
+	if payPerUsePrice > car.MaxPrice() {
+		payPerUsePrice = car.MaxPrice()
+	}
+
+	// 6時間毎の上限課金額を追加して返す
+	return int(minutes/360)*car.MaxPrice() + payPerUsePrice
 }
