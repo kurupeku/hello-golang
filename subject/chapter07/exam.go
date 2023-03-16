60984725a4edcf8e00bb5a6111f959c1e394b898
shigezou46 <16203911+ShigekiNagura@users.noreply.github.com>
2023-03-16 02:52:34 +0000

    chapter07

--- subject/chapter07/exam.go
+++ subject/chapter07/exam.go
@@ -1,6 +1,7 @@
 package chapter07
 
 import (
+	"fmt"
 	"sort"
 )
 
@@ -27,9 +28,36 @@ func (c *Casher) itemNames() []string {
 
 func (c *Casher) Purchase(item *Item) {
 	// TODO: 実装
+
+	// c.Listがnilの時は初期化
+	if c.List == nil {
+		c.List = map[string]int{}
+	}
+
+	// すでに商品が存在している場合は、加算処理
+	if _, ok := c.List[item.Name]; ok {
+		c.List[item.Name] += item.Price
+		c.TotalPrice += item.Price
+		return
+	}
+	// 存在しない場合はマップに追加する
+	c.List[item.Name] = item.Price
+	c.TotalPrice += item.Price
 }
 
 func (c *Casher) Receipt() string {
 	// TODO: 実装
-	return ""
+	// c.Listをもとにレシートを生成する
+	itemsNames := c.itemNames()
+	s := fmt.Sprint("\n")
+	s += fmt.Sprint("ラーメン道 楽酢\n")
+	s += fmt.Sprintf("\n")
+	for _, v := range itemsNames {
+		s += fmt.Sprintf("%-10s:%9d\n", v, c.List[v])
+	}
+	s += fmt.Sprintf("--------------------\n")
+	s += fmt.Sprintf("%20d", c.TotalPrice)
+	s += fmt.Sprint("\n")
+	fmt.Printf("%s", s)
+	return s
 }
