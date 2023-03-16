d89ca21f87026bcb54fa21d23206994c4b297fac
shigezou46 <16203911+ShigekiNagura@users.noreply.github.com>
2023-03-16 02:50:58 +0000

    chapter04

--- subject/chapter04/exam.go
+++ subject/chapter04/exam.go
@@ -7,5 +7,23 @@ type Card struct {
 
 func Kaisatsu(charge int, card *Card) bool {
 	// TODO: 実装
-	return false
+
+	// 支払い残高
+	paymentBalance := charge
+
+	// 残高とポイントの合計が乗車料金以下の場合は乗車できない
+	if card.Balance+card.Point < charge {
+		return false
+	}
+
+	// ポイントから優先的に支払う
+	if card.Point >= charge {
+		card.Point -= charge
+	} else {
+		paymentBalance -= card.Point
+		card.Point = 0
+		card.Balance -= paymentBalance
+	}
+
+	return true
 }
