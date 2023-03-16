3fb0d110db2964009499393e87c00bfadf58397f
shigezou46 <16203911+ShigekiNagura@users.noreply.github.com>
2023-03-16 02:52:22 +0000

    chapter06

--- subject/chapter06/exam.go
+++ subject/chapter06/exam.go
@@ -2,6 +2,7 @@ package chapter06
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/kurupeku/hello-golang/helper"
 )
@@ -18,10 +19,20 @@ func (n AhoNumber) aho() string {
 
 func (n AhoNumber) Call() string {
 	// TODO: 実装
-	return ""
+	s := n.String()
+	if n%3 == 0 || strings.Contains(s, "3") {
+		return n.aho()
+	}
+	return s
 }
 
 func Nabeatsu(n int) []string {
 	// TODO: 実装
-	return []string{}
+	var ret []string
+	for i := 1; i <= n; i++ {
+		ahoNum := AhoNumber(i)
+		s := ahoNum.Call()
+		ret = append(ret, s)
+	}
+	return ret
 }
